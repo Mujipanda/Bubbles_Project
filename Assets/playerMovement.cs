c370a9fddfd2c7ef9e6245ccf5c0f91fee5c3fb3
Muji <rasmusiam@gmail.com>
2023-10-06 01:11:19 +0100

    camera done

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -19,7 +19,15 @@ public class playerMovement : MonoBehaviour
     [Range(0.0f, 0.1f)]
     private float acceleration; // of seconds / less is faster
 
+
+    [SerializeField]
+    [Range(1.0f, 5.0f)]
+    private float sensitivity;
+
+    private float camX, camY;
     private bool delayBool = true;
+
+    public GameObject camObj;
 
     private void Awake()
     {
@@ -43,18 +51,6 @@ public class playerMovement : MonoBehaviour
         //Debug.Log(vec3.x + " X");
         //Debug.Log(vec3.z + " Z");
     }
-    private void FixedUpdate() // runs incync with each frame/ runs 50 calls a second / 25fps runs twice per frame and so on
-    {
-        //Debug.Log(vec3.x);
-        // rb.velocity = vec3 * speedx; // move the player
-        var transVec = transform.rotation * (vec3 * speedx);
-
-    
-        // rb.AddForce(vec3 * speedx);
-        rb.velocity = transVec;
-
-    }
-
     void movementMath()
     {
         // Debug.Log(speedx + " speedx");
@@ -74,7 +70,7 @@ public class playerMovement : MonoBehaviour
              }
          }*/
 
-        if (vec3.x > joyStickDeadZone || vec3.x < -joyStickDeadZone || vec3.z > joyStickDeadZone || vec3.z < -joyStickDeadZone)
+        if (vec3.x > joyStickDeadZone || vec3.x < -joyStickDeadZone || vec3.z > joyStickDeadZone || vec3.z < -joyStickDeadZone)// if more than deadZone // I dislike how long it is ;(
         {
             if (speedx < maxSpeed && delayBool) // increase speedx till max speed
             {
@@ -82,46 +78,72 @@ public class playerMovement : MonoBehaviour
             }
 
         }
-
-        else if (vec3.x == 0 || vec3.z == 0) // when still speedz returns to zero
+        else if (vec3.x == 0 || vec3.z == 0) // when still speedx returns to zero
         {
             if (speedx > 0)
             {
                 speedx -= 1;
             }
         }
-
         // Debug.Log(speedx);
         //Debug.Log(vec3);
-
-        /* if (vec3.x != 0 || vec3.z != 0) // when moving increases speedx /less optimal but prevents bugs 
- {
-     if (speedx < maxSpeed) // increase speedx till max speed
-     {
-         speedx += 1;
-     }
- }*/
     }
-
-    private IEnumerator speedxDelay()
+    private IEnumerator speedxDelay()// delay to the speed increase so its consistant
     {
-        delayBool = false;
+        delayBool = false; // bool prevents the function from running before the previous attempt has finished
         speedx += 1;
         yield return new WaitForSeconds(acceleration);
         delayBool = true;
     }
+    void OnLook(InputValue input)// looking joystick
+    {
+        Vector2 look = input.Get<Vector2>();
 
+        // Quaternion rotation = angle.Get<Quaternion>();
+
+        lookVec = new Vector3(look.x, look.y, 0);
+       // Debug.Log(lookVec.x + " X " + lookVec.y + " Y ");
+    }
+    void camLook()
+    {
+        if (lookVec.x > 0)
+        {
+            camX -= sensitivity;
+        }
+        else if (lookVec.x < 0)
+        {
+            camX += sensitivity;
+        }
+        else if(lookVec.y > 0)
+        {
+            camY -= sensitivity;
+        }
+        else if(lookVec.y < 0)
+        {
+            camY += sensitivity;
+        }
+    }
     private void Update()// works every single frame
     {
+
         movementMath();
+        //camLook();
     }
 
-
-    void OnLook(InputValue pos)
+    private void FixedUpdate() // runs incync with each frame/ runs 50 calls a second / 25fps runs twice per frame and so on
     {
-        Vector2 vecPos = pos.Get<Vector2>();
 
-        lookVec = new Vector3(vecPos.x, vecPos.y);
-        Debug.Log("right analog stick moving");
+        var transVec = transform.rotation * (vec3 * speedx); // turns vec into a transfrom( transforms are local space) 
+        rb.velocity = transVec;// move the player
+
+        camObj.transform.localEulerAngles += new Vector3(-lookVec.y * sensitivity, 0, 0);
+        gameObject.transform.localEulerAngles += new Vector3(0, lookVec.x * sensitivity, 0);
+       // gameObject.transform.localEulerAngles 
+        //gameObject.transform.rotation = Quaternion.Euler(0, -camX, 0);
+        
+
+       
     }
+
+
 }
