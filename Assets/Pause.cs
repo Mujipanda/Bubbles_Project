2c0e67e4bf0e51a050907e256419998d16ca70f0
David1911032 <128365718+David1911032@users.noreply.github.com>
2023-10-23 14:25:08 +0100

    Pause added
    Pause button and function added

---
+++ Assets/Pause.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject PauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void PauseGame()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+}
